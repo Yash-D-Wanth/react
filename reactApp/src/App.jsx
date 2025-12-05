92d43c51004f171c88bfa6dad35e739d0e24c43d
Yash-D-Wanth <yashjaiganesh@gmail.com>
2025-12-05 23:14:28 +0530

    class Component

---
+++ reactApp/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Props from './Class/Props'
+import State from './Class/State'
+function App() {
+  return (
+    <div>
+      <Props name="Yashwanth"/>
+      <State />
+    </div>
+  )
+}
+
+export default App
