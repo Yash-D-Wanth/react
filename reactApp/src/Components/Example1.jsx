5eda6420467c6933aeac45d8c17969269f90077f
Yash-D-Wanth <yashjaiganesh@gmail.com>
2025-12-05 14:00:28 +0530

    functional and class components basics

---
+++ reactApp/src/Components/Example1.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Example1 extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.name} is mastering React!
+      </div>
+    )
+  }
+}
+
+export default Example1
---
+++ reactApp/src/Components/Example2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Example2(props) {
+  return (
+    <div>
+        {props.name} is learning React Functional Components!
+    </div>
+  )
+}
+
+export default Example2
