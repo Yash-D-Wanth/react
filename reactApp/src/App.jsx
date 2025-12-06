eabec1db00a5927e9166ac9bdafa3af8c3b292d9
Yash-D-Wanth <yashjaiganesh@gmail.com>
2025-12-06 23:08:52 +0530

    updated

--- reactApp/src/App.jsx
+++ reactApp/src/App.jsx
@@ -5,14 +5,16 @@
 // import Conditional from './Functional/Conditional'
 // import Event from './Functional/Event'
 // import Array from "./Functional/Array"
-import Objects from "./Functional/Objects"
+// import Objects from "./Functional/Objects"
 function App() {
-    // const person=["davy D jones","joyboy","monkey D luffy"]
-    const characters={
-      name:"jon snow",
-      age:24,
-      blood:"targaryen",
-    }
+    // const person=["davy D jones","joyboy","monkey D luffy"]                 {array}
+
+    
+    // const characters={                                                           \ 
+    //   name:"jon snow",                                                           \
+    //   age:24,                                                              {objects}
+    //   blood:"targaryen as well as stark",                                         \
+    // }
   return (
     <>
       {/* <Props name="React Develope */}
@@ -20,7 +22,7 @@ function App() {
       {/* <Event /> */}
         {/* <Conditional /> */}
         {/* <Array person={person}/> */}
-        <Objects characters={characters}/>
+        {/* <Objects characters={characters}/> */}
     </>
   )
 }
