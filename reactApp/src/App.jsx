fbd6791468bf44f9c3c49dc2d185993ff2972970
Yash-D-Wanth <yashjaiganesh@gmail.com>
2025-12-06 23:18:29 +0530

    updated

--- reactApp/src/App.jsx
+++ reactApp/src/App.jsx
@@ -4,15 +4,15 @@ import React from 'react'
 // import Event from './class/Event.jsx'
 // import Conditional from './class/Conditional'
 // import Array from './class/Array.jsx'
-import Objects from './class/Objects.jsx'
+// import Objects from './class/Objects.jsx'
 function App() {
   // const persons=["jonathan","steve","hopper"]                   {array}
 
-  const characters={                                            //{object}
-    name:"jesse pinkman",
-    age:50,
-    series:"breaking bad"
-  }
+  // const characters={                                            //{object}
+  //   name:"jesse pinkman",
+  //   age:50,
+  //   series:"breaking bad"
+  // }
   return (
     <div>
       {/* <Props name="Yashwanth"/>
@@ -20,7 +20,7 @@ function App() {
       {/* <Event /> */}
         {/* <Conditional /> */}
         {/* <Array persons={persons} /> */}
-        <Objects characters={characters} />  
+        {/* <Objects characters={characters} />   */}
     </div>
   )
 }
