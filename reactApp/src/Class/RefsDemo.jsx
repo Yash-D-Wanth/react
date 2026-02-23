24550f16d49477f79f1949d1c9ff9e0ba886f3fd
Yash-D-Wanth <yashjaiganesh@gmail.com>
2026-02-23 19:24:09 +0530

    mine

--- reactApp/src/Class/RefsDemo.jsx
+++ reactApp/src/Class/RefsDemo.jsx
@@ -2,34 +2,32 @@ import React,{Component} from 'react'
 class RefsDemo extends Component{
     constructor(){
         super()
-        // this.inputRef=React.createRef()
-        this.cbRef=null
+        this.inputRef=React.createRef()
+        this.cbRef=null;
         this.setCbRef=(element)=>{
             this.cbRef=element
         }
     }
     componentDidMount(){
-        // this.inputRef.current.focus()
+        this.inputRef.current.focus()
+        // if(this.cbRef){
+        //     this.cbRef.focus()
+        // }
         // console.log(this.inputRef)
-        if(this.cbRef){
-            this.cbRef.focus()
-        }
     }
     clickHandler=()=>{
-        // alert(this.inputRef.current.value)
+        console.log(this.inputRef.current.value)
         console.log(this.cbRef.value)
-        alert(this.cbRef.value)
-
     }
     render(){
         return(
             <>
-            <label>for ref:</label><br/>
-            <input type="text" ref={this.inputRef}/>
-            <br></br>
-            <label>for Call back Ref:</label><br/>
-            <input type="text" ref={this.setCbRef}/><br/>
+            <label>for ref:</label><br></br>
+            <input type="text" ref={this.inputRef}/><br/>
+            <label>for cb ref:</label><br/>
+            <input type="text" ref={this.setCbRef}/>
             <button onClick={this.clickHandler}>click</button>
+
             </>
         )
     }
